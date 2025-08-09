49c78a705c830b4943586668ab145452d23beacb
RosaUlisses <ulissao@proton.me>
2025-08-09 13:52:39 -0300

    final scripts

--- src/chimera_testing/get-size.sh
+++ src/chimera_testing/get-size.sh
@@ -1,11 +1,5 @@
 for design in /home/ullas/manticore/manticore/scripts/vectorizable-designs/default/*.v; do
   echo $design
-  ./run-vectorization.sh $design
+  ./run-vectorization.sh $design 2>&1 | grep "Detected" >> logs.txt
   rm -rf *.mlir
 done
-
-
-
-
-
-
---
+++ src/chimera_testing/jasper-commands.tcl
@@ -0,0 +1,3 @@
+analyze -sv ../../scripts/vectorizable-designs/vectorized/45238_synlig_tests_bsg_micro_designs_results_bsg_permute_box.json_gold.v
+elaborate
+exit 0
