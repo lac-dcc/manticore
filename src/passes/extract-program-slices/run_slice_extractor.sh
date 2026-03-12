38a3e040c6a9447d1465f8783b09d6a03edd3a5b
rafaelpaniago1 <rafaelpradopaniago@gmail.com>
2026-03-12 10:09:45 -0300

    Updated Makefile and bash script

--- src/passes/extract-program-slices/run_slice_extractor.sh
+++ src/passes/extract-program-slices/run_slice_extractor.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-BASE_NAME="test_extractor"
+INPUT_DIR="./benchmarking/input_verilog/test_extractor"
+TMP_MLIR_DIR="./benchmarking/tmp_mlir/test_extractor"
+OUTPUT_DIR="./benchmarking/generated_verilog/test_extractor"
 PLUGIN_DIR="./build" 
-CIRCT_BIN=~/projects/circt/build/bin
+# Correcting for general path 
+CIRCT_BIN="$HOME/circt/build/bin"
 
 echo "Converting Verilog to HW"
-circt-verilog --ir-hw ${BASE_NAME}.v -o ${BASE_NAME}.hw.mlir
 
+circt-verilog --ir-hw ${INPUT_DIR}.v -o ${TMP_MLIR_DIR}.hw.mlir
 echo "Running HW Slice Extractor pass"
-$CIRCT_BIN/circt-opt ${BASE_NAME}.hw.mlir \
-  --pass-pipeline="builtin.module(slice-extractor)" \
+$CIRCT_BIN/circt-opt ${TMP_MLIR_DIR}.hw.mlir \
   --load-pass-plugin=${PLUGIN_DIR}/SliceExtractorPass.so \
-  -o ${BASE_NAME}.outlined.mlir
+  --pass-pipeline="builtin.module(slice-extractor)" \
+  -o ${TMP_MLIR_DIR}.outlined.mlir
 
-$CIRCT_BIN/circt-opt ${BASE_NAME}.outlined.mlir \
+echo "Cleaning up MLIR"
+$CIRCT_BIN/circt-opt ${TMP_MLIR_DIR}.outlined.mlir \
+  --convert-moore-to-core \
   --canonicalize \
   --cse \
   --prettify-verilog \
   --symbol-dce \
-  -o ${BASE_NAME}.cleaned.mlir
+  -o ${TMP_MLIR_DIR}.cleaned.mlir
 
 echo "Generating final Verilog"
-$CIRCT_BIN/firtool ${BASE_NAME}.cleaned.mlir \
+
+$CIRCT_BIN/firtool ${TMP_MLIR_DIR}.cleaned.mlir \
   --verilog \
   --disable-all-randomization \
-  -o ${BASE_NAME}_final.v
+  -o ${OUTPUT_DIR}_final.v
 
-echo "Verilog generated in ${BASE_NAME}_final.v"
+echo "Verilog generated in ${OUTPUT_DIR}_final.v"
+
