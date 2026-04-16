372143030d4fc6cccbfe0948d718ea0b466befb8
rafaelpaniago1 <rafaelpradopaniago@gmail.com>
2026-04-16 15:42:37 -0300

    Moved CareMaskAnalysis.cpp file to the main lib directory

--- src/passes/extract-program-slices/lib/CareMaskAnalysis.cpp
+++ src/passes/extract-program-slices/lib/CareMaskAnalysis.cpp
@@ -1,4 +1,4 @@
-#include "../../include/CareMaskAnalysis.hpp"
+#include "../include/CareMaskAnalysis.hpp"
 #include "circt/Dialect/Comb/CombOps.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "mlir/Analysis/DataFlow/SparseAnalysis.h"
